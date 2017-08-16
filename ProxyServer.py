05d7828253903e5b5cd252c7321f00bda041b21b
Evgeniy Evgenich <evgeniysgs3@gmail.com>
2017-08-17 00:28:12 +0300

    Испралено логирование, но проблема не выявлена, по прежнему логи пересекаются, хотя объявлено два разных логера

--- ProxyServer.py
+++ ProxyServer.py
@@ -10,7 +10,9 @@ import time
 import json
 import sys
 from multiprocessing import Process
-import os, platform, logging
+import os
+import platform
+import logging
 import socket
 from urllib.parse import urlparse
 from adminka.adminka.source_code.constants import *
@@ -19,14 +21,40 @@ from adminka.adminka.source_code.clientAndHotspot import Client, Hotspot
 # Словарь для создания буфера записи лога и обновлений строк User-Agent в БД
 dic_ip_user_agent = {}
 
-# Настройка лог файла
-logging.basicConfig(
-    level=LOG_LEVEL,
-    format=u'[%(asctime)s] %(levelname)-8s  %(message)s (%(filename)s)',
-    filename=LOGS_DIR + u'main.log',
-    filemode='w',
-)
-logging_file = LOGS_DIR + u'log_proxy_server.log'
+# Настройка лог файлаов
+
+
+def setup_logger(logger_name, log_file, level=logging.INFO):
+    l = logging.getLogger(logger_name)
+    formatter = logging.Formatter(u'[%(asctime)s] %(levelname)-8s  %(message)s (%(filename)s)')
+    fileHandler = logging.FileHandler(log_file, mode='w')
+    fileHandler.setFormatter(formatter)
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+
+    l.setLevel(level)
+    l.addHandler(fileHandler)
+    l.addHandler(streamHandler)
+
+# main.log
+PROXY_SERVER_MAIN = LOGS_DIR + u'main.log'
+setup_logger('MainLog', PROXY_SERVER_MAIN)
+main_proxy_server = logging.getLogger('MainLog')
+# hdlr_main = logging.FileHandler(PROXY_SERVER_MAIN)
+# formatter = logging.Formatter(u'[%(asctime)s] %(levelname)-8s  %(message)s (%(filename)s)')
+# hdlr_main.setFormatter(formatter)
+# main_proxy_server.addHandler(hdlr_main)
+# main_proxy_server.setLevel(logging.INFO)
+
+# log_proxy_server.log
+LOG_PROXY_SERVER = LOGS_DIR + u'log_proxy_server.log'
+setup_logger('LogProxyServer', LOG_PROXY_SERVER, level=logging.DEBUG)
+log_proxy_server = logging.getLogger('LogProxyServer')
+# hdlr_ps = logging.FileHandler(LOG_PROXY_SERVER)
+# formatter_ps = logging.Formatter(u'[%(asctime)s] %(levelname)-8s  %(message)s (%(filename)s)')
+# hdlr_ps.setFormatter(formatter_ps)
+# log_proxy_server.addHandler(hdlr_ps)
+# log_proxy_server.setLevel(logging.DEBUG)
 
 __all__ = ['ProxyHandler', 'run_proxy']
 
@@ -75,16 +103,9 @@ class ProxyHandler(tornado.web.RequestHandler):
     def get(self):
 
         # Сбор статистики в лог файл
-        #msg_log = {}
-        #msg_log["IP"] = self.request.remote_ip
-        #msg_log["User-Agent"] = self.request.headers['User-Agent']
-        #msg_log = self.request.remote_ip + ' : ' + self.request.headers['User-Agent']
-        #logging.info(json.dumps(msg_log))
         global dic_ip_user_agent
         dic_ip_user_agent[self.request.remote_ip] = self.request.headers['User-Agent']
-
-        #logging.info('Handle %s request to %s', self.request.method,\
-        #             self.request.uri)
+        log_proxy_server.debug('МЕТОД %s URL %s', self.request.method, self.request.uri)
 
         def handle_response(response):
             if (response.error and not isinstance(response.error, tornado.httpclient.HTTPError)):
@@ -93,29 +114,27 @@ class ProxyHandler(tornado.web.RequestHandler):
             else:
                 self.set_status(response.code, response.reason)
                 self._headers = tornado.httputil.HTTPHeaders()  # clear tornado default header
-                #logging.info("Header response: {}".format(response.headers.get_all()))
+                log_proxy_server.debug("ЗАГОЛОВОК ОТВЕТА: {}".format(response.headers.get_all()))
                 for header, v in response.headers.get_all():
                     if header not in ('Content-Length', 'Transfer-Encoding', 'Content-Encoding', 'Connection'):
                         self.add_header(header, v)  # some header appear multiple times, eg 'Set-Cookie'
 
-                #logging.info("Header request: {}".format(self.request.headers))
+                log_proxy_server.debug("ЗАГОЛОВОК ЗАПРОСА: {}".format(self.request.headers))
 
                 if (response.body and self.mode == 'jsinj'):
                     self.set_header('Content-Length', len(response.body) + len(self.jscript))
                     self.write(response.body + self.jscript)
                     # Отчет о внедрении JavaScript
-                    #logging.info('JS INJECTED - Пользователю с IP: ' + self.request.remote_ip + 'JS внедрен',\
-                    #            filename=LoggingProxyServer.logging_file)
-                    #print(LoggingProxyServer.logging_file)
-                    #logging.info("Body response: {}".format(response.body))
+                    main_proxy_server.info('JS INJECTED - Пользователю с IP: ' + self.request.remote_ip + 'JS внедрен')
+                    log_proxy_server.debug("ТЕЛО ОТВЕТА: {}".format(response.body))
                 elif response.body:
                     self.set_header('Content-Length', len(response.body))
                     self.write(response.body)
-                    #logging.info("Body response: {}".format(response.body))
+                    log_proxy_server.debug("ТЕЛО ОТВЕТА: {}".format(response.body))
             self.finish()
 
         body = self.request.body
-        #logging.info("Body request: {}".format(body))
+        log_proxy_server.debug("ТЕЛО ЗАПРОСА: {}".format(body))
         if not body:
             body = None
         try:
@@ -130,7 +149,7 @@ class ProxyHandler(tornado.web.RequestHandler):
             if hasattr(e, 'response') and e.response:
                 handle_response(e.response)
             else:
-                logging.error('ОШИБКА - Внутренняя ошибка прокси-сервера')
+                main_proxy_server.error('ОШИБКА - Внутренняя ошибка прокси-сервера')
                 self.set_status(500)
                 self.write('Internal server error:\n' + str(e))
                 self.finish()
@@ -251,7 +270,7 @@ class Configuration:
         self.mode = None
         self.jscript = None
 
-    def setconfig(self, port, mode, jscript):
+    def setconfig(self, port, mode=None, jscript=None):
         self.port = port
         self.mode = mode
         self.jscript = jscript
@@ -268,6 +287,7 @@ class ProxyServer:
         try:
             if mode == 'jsinj':
                 payload_js = get_js_for_inj(jscript)
+                main_proxy_server.info("PROXY_SERVER запущен в режиме JSINJECTION")
                 app = tornado.web.Application([
                     (r'.*', ProxyHandler, dict(m=mode, j=payload_js))
                 ])
@@ -277,11 +297,12 @@ class ProxyServer:
                     (r'.*', ProxyHandler, dict(m=mode))
                 ])
             else:
+                main_proxy_server.info("PROXY_SERVER запущен в режиме СБОР_СТАТИСТИКИ")
                 app = tornado.web.Application([
                     (r'.*', ProxyHandler),
                 ])
         except Exception as e:
-            logging.error('Неверные параметры скрипта при запуске Proxy Server')
+            main_proxy_server.error('Неверные параметры скрипта при запуске PROXY_SERVER')
             sys.exit(1)
 
         app.listen(port)
@@ -300,27 +321,32 @@ class WorkerProxyServer:
     def start(self):
 
         global config
-        print(config.port)
+        # print(config.port)
         process_proxy = Process(target=self.proxy, args=(config.port, config.mode, config.jscript))
         process_proxy.start()
-        logging.info('ProxyServer запустился с PID = {0}'.format(process_proxy.pid))
+        main_proxy_server.info('PROXY_SERVER запустился с PID = {0}'.format(process_proxy.pid))
         self.pid_proxy = process_proxy.pid
-        print("PID = {0}".format(process_proxy.pid))
+        #print("PID = {0}".format(process_proxy.pid))
         signal.signal(signal.SIGUSR1, WorkerProxyServer.on_exit)
 
     def stop(self):
         """Останавливает работу прокси сервера вместе со всеми дочерними процессами"""
+        main_proxy_server.info('PROXY_SERVER остановлен')
         os.kill(int(self.pid_proxy), signal.SIGUSR1)
 
     def on_exit(self):
-        logging.info('ProxyServer остановлен')
+        #main_proxy_server.info('PROXY_SERVER остановлен')
         sys.exit(0)
 
 
 if __name__ == '__main__':
 
     config = Configuration()
-    config.setconfig(port=8080, mode='jsinj', jscript='test_js')
+    # СБОР_СТАТИСТИКИ
+    config.setconfig(port=8080)
+
+    # JSINJECTION
+    #config.setconfig(port=8080, mode='jsinj', jscript='test_js')
 
     workProxyServer = WorkerProxyServer()
     workProxyServer.start()
